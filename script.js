6a12243a5becce54d146102e0be6fdd1e683d020
Borstan28 <86780067+Borstan28@users.noreply.github.com>
2021-07-01 14:27:51 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,644 @@
+let hiragana = [
+    {
+      "kana": "あ",
+      "roumaji": "a",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "い",
+      "roumaji": "i",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "う",
+      "roumaji": "u",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "え",
+      "roumaji": "e",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "お",
+      "roumaji": "o",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "か",
+      "roumaji": "ka",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "き",
+      "roumaji": "ki",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "く",
+      "roumaji": "ku",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "け",
+      "roumaji": "ke",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "こ",
+      "roumaji": "ko",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "さ",
+      "roumaji": "sa",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "し",
+      "roumaji": "shi",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "す",
+      "roumaji": "su",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "せ",
+      "roumaji": "se",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "そ",
+      "roumaji": "so",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "た",
+      "roumaji": "ta",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ち",
+      "roumaji": "chi",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "つ",
+      "roumaji": "tsu",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "て",
+      "roumaji": "te",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "と",
+      "roumaji": "to",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "な",
+      "roumaji": "na",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "に",
+      "roumaji": "ni",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ぬ",
+      "roumaji": "nu",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ね",
+      "roumaji": "ne",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "の",
+      "roumaji": "no",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "は",
+      "roumaji": "ha",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ひ",
+      "roumaji": "hi",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ふ",
+      "roumaji": "hu",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "へ",
+      "roumaji": "he",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ほ",
+      "roumaji": "ho",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ま",
+      "roumaji": "ma",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "み",
+      "roumaji": "mi",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "む",
+      "roumaji": "mu",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "め",
+      "roumaji": "me",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "も",
+      "roumaji": "mo",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "や",
+      "roumaji": "ya",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ゆ",
+      "roumaji": "yu",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "よ",
+      "roumaji": "yo",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ら",
+      "roumaji": "ra",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "り",
+      "roumaji": "ri",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "る",
+      "roumaji": "ru",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "れ",
+      "roumaji": "re",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ろ",
+      "roumaji": "ro",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "わ",
+      "roumaji": "wa",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "を",
+      "roumaji": "wo",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "ん",
+      "roumaji": "n",
+      "type": "gojuuon"
+    },
+    {
+      "kana": "が",
+      "roumaji": "ga",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ぎ",
+      "roumaji": "gi",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ぐ",
+      "roumaji": "gu",
+      "type": "dakuon"
+    },
+    {
+      "kana": "げ",
+      "roumaji": "ge",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ご",
+      "roumaji": "go",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ざ",
+      "roumaji": "za",
+      "type": "dakuon"
+    },
+    {
+      "kana": "じ",
+      "roumaji": "ji",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ず",
+      "roumaji": "zu",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ぜ",
+      "roumaji": "ze",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ぞ",
+      "roumaji": "zo",
+      "type": "dakuon"
+    },
+    {
+      "kana": "だ",
+      "roumaji": "da",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ぢ",
+      "roumaji": "ji",
+      "type": "dakuon"
+    },
+    {
+      "kana": "づ",
+      "roumaji": "zu",
+      "type": "dakuon"
+    },
+    {
+      "kana": "で",
+      "roumaji": "de",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ど",
+      "roumaji": "do",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ば",
+      "roumaji": "ba",
+      "type": "dakuon"
+    },
+    {
+      "kana": "び",
+      "roumaji": "bi",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ぶ",
+      "roumaji": "bu",
+      "type": "dakuon"
+    },
+    {
+      "kana": "べ",
+      "roumaji": "be",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ぼ",
+      "roumaji": "bo",
+      "type": "dakuon"
+    },
+    {
+      "kana": "ぱ",
+      "roumaji": "pa",
+      "type": "handakuon"
+    },
+    {
+      "kana": "ぴ",
+      "roumaji": "pi",
+      "type": "handakuon"
+    },
+    {
+      "kana": "ぷ",
+      "roumaji": "pu",
+      "type": "handakuon"
+    },
+    {
+      "kana": "ぺ",
+      "roumaji": "pe",
+      "type": "handakuon"
+    },
+    {
+      "kana": "ぽ",
+      "roumaji": "po",
+      "type": "handakuon"
+    },
+    {
+      "kana": "っ",
+      "roumaji": "(pause)",
+      "type": "sokuon"
+    },
+    {
+      "kana": "きゃ",
+      "roumaji": "kya",
+      "type": "youon"
+    },
+    {
+      "kana": "きゅ",
+      "roumaji": "kyu",
+      "type": "youon"
+    },
+    {
+      "kana": "きょ",
+      "roumaji": "kyo",
+      "type": "youon"
+    },
+    {
+      "kana": "しゃ",
+      "roumaji": "sha",
+      "type": "youon"
+    },
+    {
+      "kana": "しゅ",
+      "roumaji": "shu",
+      "type": "youon"
+    },
+    {
+      "kana": "しょ",
+      "roumaji": "sho",
+      "type": "youon"
+    },
+    {
+      "kana": "ちゃ",
+      "roumaji": "cha",
+      "type": "youon"
+    },
+    {
+      "kana": "ちゅ",
+      "roumaji": "chu",
+      "type": "youon"
+    },
+    {
+      "kana": "ちょ",
+      "roumaji": "cho",
+      "type": "youon"
+    },
+    {
+      "kana": "にゃ",
+      "roumaji": "nya",
+      "type": "youon"
+    },
+    {
+      "kana": "にゅ",
+      "roumaji": "nyu",
+      "type": "youon"
+    },
+    {
+      "kana": "にょ",
+      "roumaji": "nyo",
+      "type": "youon"
+    },
+    {
+      "kana": "ひゃ",
+      "roumaji": "hya",
+      "type": "youon"
+    },
+    {
+      "kana": "ひゅ",
+      "roumaji": "hyu",
+      "type": "youon"
+    },
+    {
+      "kana": "ひょ",
+      "roumaji": "hyo",
+      "type": "youon"
+    },
+    {
+      "kana": "みゃ",
+      "roumaji": "mya",
+      "type": "youon"
+    },
+    {
+      "kana": "みゅ",
+      "roumaji": "myu",
+      "type": "youon"
+    },
+    {
+      "kana": "みょ",
+      "roumaji": "myo",
+      "type": "youon"
+    },
+    {
+      "kana": "りゃ",
+      "roumaji": "rya",
+      "type": "youon"
+    },
+    {
+      "kana": "りゅ",
+      "roumaji": "ryu",
+      "type": "youon"
+    },
+    {
+      "kana": "りょ",
+      "roumaji": "ryo",
+      "type": "youon"
+    },
+    {
+      "kana": "ぎゃ",
+      "roumaji": "gya",
+      "type": "youon"
+    },
+    {
+      "kana": "ぎゅ",
+      "roumaji": "gyu",
+      "type": "youon"
+    },
+    {
+      "kana": "ぎょ",
+      "roumaji": "gyo",
+      "type": "youon"
+    },
+    {
+      "kana": "じゃ",
+      "roumaji": "ja",
+      "type": "youon"
+    },
+    {
+      "kana": "じゅ",
+      "roumaji": "ju",
+      "type": "youon"
+    },
+    {
+      "kana": "じょ",
+      "roumaji": "jo",
+      "type": "youon"
+    },
+    {
+      "kana": "びゃ",
+      "roumaji": "bya",
+      "type": "youon"
+    },
+    {
+      "kana": "びゅ",
+      "roumaji": "byu",
+      "type": "youon"
+    },
+    {
+      "kana": "びょ",
+      "roumaji": "byo",
+      "type": "youon"
+    },
+    {
+      "kana": "ぴゃ",
+      "roumaji": "pya",
+      "type": "youon"
+    },
+    {
+      "kana": "ぴゅ",
+      "roumaji": "pyu",
+      "type": "youon"
+    },
+    {
+      "kana": "ぴょ",
+      "roumaji": "pyo",
+      "type": "youon"
+    }
+  ]
+let gomodal = document.querySelector('.modalGameOver');
+let cont = document.querySelector('.container')
+let plates = document.querySelectorAll('.sampleImg');
+let timer = document.querySelector('.timer');
+let button = document.querySelector('.buttonStart');
+let questionDiv = document.querySelector('.question')
+let onTimer = false;
+let currentTimer = parseInt(timer.style.width);
+let currentQuestion;
+let a;
+let cardsArray = [];
+let pointsDiv = document.querySelector('.points');
+let points = 0;
+let startButton = document.querySelector('.start');
+
+function updatePoints(points){
+  pointsDiv.innerHTML = points;
+}
+
+function replayButton(){
+  gomodal.style.visibility = 'hidden';
+  startButton.style.visibility = 'visible';
+}
+
+function setTimer(){
+    if(onTimer==false){
+        generateCards();
+        chooseQuestion();
+        startButton.style.visibility = 'hidden';
+        onTimer = true;
+        a = setInterval(()=>{
+            timer.style.width = `${currentTimer-=3}px`;
+            console.log(currentTimer)
+            if(currentTimer<=0){
+                clearInterval(a);
+                cardsArray = [];
+                timer.style.width = '300px';
+                onTimer = false;
+                cont.innerHTML = '';
+                questionDiv.innerHTML = 'Press Start'
+                currentTimer = parseInt(timer.style.width);
+                gomodal.style.visibility = 'visible';
+            } else if(cardsArray.length==0){
+              alert('you won');
+              clearInterval(a);
+              timer.style.width = '300px';
+              currentTimer = parseInt(timer.style.width);
+              onTimer = false;
+              cont.innerHTML = '';
+              setTimeout(()=>{
+                generateCards();
+                chooseQuestion();
+              }, 100);
+            }
+        },100)
+    } else if(onTimer==true){
+        onTimer = false;
+        clearInterval(a)
+        cont.innerHTML = '';
+        cardsArray = []
+    }
+}
+
+function generateCards(){
+    let randomPick = [];
+    for(let i = 0; i<=14 ; i++){
+        let result = Math.floor(Math.random()*92);
+        if(cardsArray.includes(hiragana[result])){
+            cardsArray.push(hiragana[result+1])
+        } else 
+        cardsArray.push(hiragana[result])
+    }
+    console.log(cardsArray)
+    cardsArray.forEach(item => {
+        let card = document.createElement('div');
+        card.classList = 'sampleImg';
+        card.innerHTML = item.kana;
+        if(card.innerHTML.length>1){
+            card.style.fontSize = '90px';
+        }
+        card.id = item.roumaji;
+        card.addEventListener('click',()=>{
+            if(card.id == currentQuestion && card.style.backgroundColor !== 'white'){
+                currentTimer+=50;
+                points+=5;
+                updatePoints(points)
+                timer.style.width = `${currentTimer}px`
+                card.style.backgroundColor='#a2ff8f';
+                setTimeout(()=>{
+                card.style.backgroundColor = 'white';
+                card.innerHTML=''
+                cardsArray.splice(cardsArray.indexOf(item),1)
+                chooseQuestion();
+            },150);
+            } else if(card.id !== currentQuestion && card.style.backgroundColor !== 'white'){
+              currentTimer-=25;
+              points-=3;
+              card.style.backgroundColor='red';
+              setTimeout(()=>{
+                card.style.backgroundColor = 'palevioletred'
+              },150)
+              updatePoints(points);
+            }
+                
+        })
+        cont.appendChild(card);
+        
+    })
+
+}
+
+function chooseQuestion(){
+    console.log(cardsArray)
+    let res = Math.floor(Math.random()*cardsArray.length);
+    currentQuestion = cardsArray[res].roumaji;
+    questionDiv.innerHTML = `What is ${currentQuestion.toUpperCase()}`
+}
